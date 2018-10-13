63b58da6eef5f53a28f8116cbe0c0332330c856d
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-13 00:33:22 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -3,13 +3,13 @@
 const moduleTest = require("../");
 
 var message = {};
-message.moduleName = "test";
 
 var context = {};
 context.security = {};
 context.security.credentials = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Yjk5YTBlODhmZTA0ZDM5YjRjZjAzNTMiLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE1MzcyMTk2ODUsImVtYWlsIjoiYWRtaW5AYWRtaW4iLCJleHAiOjE1MzcyMjMyODUsImlzcyI6ImZ1bmN0aW9ucy1pbyJ9.Yl_4QV7W7IjVFTH41wSPIAcJcvLK8aQ2mLp7Nu-aWBo";
+context.listModulesInvoked = ["module1"];
 
-moduleTest(message, context).then(function(result){
+moduleTest.process(message, context).then(function(result){
     console.log("sucess! ", result);
 }, function(err){
     console.log("err! ", err);
--- test/test.js
+++ test/test.js
@@ -3,9 +3,14 @@
 const assert = require("assert");
 const moduleTest = require("../");
 
-var tokenJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Yjk5YTBlODhmZTA0ZDM5YjRjZjAzNTMiLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE1MzcyMTk2ODUsImVtYWlsIjoiYWRtaW5AYWRtaW4iLCJleHAiOjE1MzcyMjMyODUsImlzcyI6ImZ1bmN0aW9ucy1pbyJ9.Yl_4QV7W7IjVFTH41wSPIAcJcvLK8aQ2mLp7Nu-aWBo";
+var message = {};
 
-moduleTest(tokenJWT).then(function(result){
+var context = {};
+context.security = {};
+context.security.credentials = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Yjk5YTBlODhmZTA0ZDM5YjRjZjAzNTMiLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE1MzcyMTk2ODUsImVtYWlsIjoiYWRtaW5AYWRtaW4iLCJleHAiOjE1MzcyMjMyODUsImlzcyI6ImZ1bmN0aW9ucy1pbyJ9.Yl_4QV7W7IjVFTH41wSPIAcJcvLK8aQ2mLp7Nu-aWBo";
+context.listModulesInvoked = ["module1"];
+
+moduleTest.process(message, context).then(function(result){
     assert.strictEqual(typeof(result), "object");
 }, function(err){
     assert.strictEqual(err, null);
